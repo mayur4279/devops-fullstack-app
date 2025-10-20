e56531f6f0342996084c1c011a362ed764238ff8
mayur4279 <mayurdhotre.a15@gmail.com>
2025-10-20 21:21:18 +0530

    code is updated

--- database/init.sh
+++ database/init.sh
@@ -1,3 +1,14 @@
+# #!/usr/bin/env bash
+# set -e
+
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#     CREATE USER mayur WITH PASSWORD '42795590';
+#     CREATE DATABASE employee_db;
+#     GRANT ALL PRIVILEGES ON DATABASE employee_db TO mayur;
+# EOSQL
+
+
+
 #!/usr/bin/env bash
 set -e
 
@@ -5,4 +16,14 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER mayur WITH PASSWORD '42795590';
     CREATE DATABASE employee_db;
     GRANT ALL PRIVILEGES ON DATABASE employee_db TO mayur;
+    
+    -- Connect to the new database and grant schema permissions
+    \c employee_db;
+    GRANT ALL ON SCHEMA public TO mayur;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mayur;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mayur;
+    
+    -- Set default privileges for future objects
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mayur;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mayur;
 EOSQL
