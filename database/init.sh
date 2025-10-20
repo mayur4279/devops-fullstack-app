1bce840b3149512f6bd9791e9daf27c463808a5d
mayur4279 <mayurdhotre.a15@gmail.com>
2025-10-20 22:32:29 +0530

    code is updated

--- database/init.sh
+++ database/init.sh
@@ -1,14 +1,3 @@
-# #!/usr/bin/env bash
-# set -e
-
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-#     CREATE USER mayur WITH PASSWORD '42795590';
-#     CREATE DATABASE employee_db;
-#     GRANT ALL PRIVILEGES ON DATABASE employee_db TO mayur;
-# EOSQL
-
-
-
 #!/usr/bin/env bash
 set -e
 
